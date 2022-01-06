cbc95960e4e56634793380717c61398b7805c160
piemree <pi.emree@gmail.com>
2022-01-06 03:20:51 +0300

    Add vercel.json file

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,8 @@ export default {
       { name: "format-detection", content: "telephone=no" },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: "preload", as: "script", href: "../nuxt/App.js" }],
+    
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
